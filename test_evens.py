8de71984cc8c4354316a00dd9fdc57aee96c0c12
Jean de Villiers <jeandevilliers@me.com>
2023-01-03 18:29:21 +0000

    Testing the Student class methods

--- test_evens.py
+++ test_evens.py
@@ -3,7 +3,7 @@ from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    
+
     def test_throws_error_if_value_passed_is_not_list(self):
         self.assertRaises(TypeError, even_number_of_evens, 4)
 
